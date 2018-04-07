2937bdc3cca658f30c95a09826d6a9e879ca8476
LongtermPartner <949196883@qq.com>
2018-04-07 19:04:39 +0800

    扩展任务

--- 排序模块/CountAndSort.java
+++ 排序模块/CountAndSort.java
@@ -1,72 +1,68 @@
 package wcPro;
 
-import java.awt.List;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.TreeMap;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-import javax.naming.ldap.SortControl;
+
 import javax.swing.JOptionPane;
-import javax.xml.bind.ParseConversionEvent;
 
 public class CountAndSort {
-	
 	public  static ArrayList<Entry<String, Integer>> countAndSort(File file) throws IOException {
 
 		TreeMap<String, Integer> hMap=parse(file.getName());
-		ArrayList<Map.Entry<String, Integer>> list=Sort(hMap);
-		
+		ArrayList<Map.Entry<String, Integer>> list=sort(hMap);
 		return list;
 		
 	}
-	
+	protected static Pattern pattern;
+	protected static Matcher matcher;
 
-		
-		//对统计的单词进行词频排序
-		public static ArrayList<Map.Entry<String, Integer>> Sort(TreeMap<String, Integer> wordsCount){
-			// 升序比较器
-			Comparator<Map.Entry<String, Integer>> valueComparator = new Comparator<Map.Entry<String,Integer>>() {
+	//对统计的单词进行词频排序
+	public static ArrayList<Map.Entry<String, Integer>> sort(TreeMap<String, Integer> wordsCount){
+		// 升序比较器
+		Comparator<Map.Entry<String, Integer>> valueComparator = new Comparator<Map.Entry<String,Integer>>() {
 
-			@Override
-			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
-			
-				return o2.getValue()-o1.getValue()==0?getInt(o2.getKey())-getInt(o1.getKey()):o2.getValue()-o1.getValue();
-			}
-
-			private int getInt(String key) {
-				
-				int i=0;
-				try{
-				Pattern pattern=Pattern.compile("^\\d+");
-				Matcher matcher=pattern.matcher(key);
-				if(matcher.find()){
-					i=Integer.valueOf(matcher.group());
-				}
-				}catch(NumberFormatException e){
-					e.printStackTrace();
-				}
-				return i;
-			}
-			};
-			
-			
-			// map转换成list进行排序
-			ArrayList<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String,Integer>>(wordsCount.entrySet());
-			// 排序
-			Collections.sort(list,valueComparator);
-
-			return list;
+		@Override
+		public int compare(Entry<String, Integer> word1, Entry<String, Integer> word2) {
+		  //若是词频相同则比较键值大小即String转化成int数值类型
+			return word2.getValue()-word1.getValue()==0?getInt(word2.getKey())-getInt(word1.getKey()):word2.getValue()-word1.getValue();
 		}
+          //将String类型转化成数值类型
+		private int getInt(String key) {
+			
+			int integer=0;
+			try{
+			pattern=Pattern.compile("^\\d+");
+			 matcher=pattern.matcher(key);
+			if(matcher.find()){
+				integer=Integer.valueOf(matcher.group());
+			}
+			}catch(NumberFormatException e){
+				e.printStackTrace();
+			}
+			return integer;
+		}
+		};
+		
+		// map转换成list进行排序
+		ArrayList<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String,Integer>>(wordsCount.entrySet());
+		// 排序
+		Collections.sort(list,valueComparator);
+		return list;
+	}
+
+
 
 }
--- 计数模块/CountAndSort.java
+++ 计数模块/CountAndSort.java
@@ -1,14 +1,13 @@
-//读取文件并解析，得到单词表（无序）
 public static TreeMap<String,Integer> parse(String filename) throws IOException{
-	FileReader fileReader=null;
-	BufferedReader bufferedReader=null;
-			
+	FileReader fileReader = null;
+	BufferedReader bufferedReader = null;
+	
 	//每行总字符串
-	String str=null;
+	String str;
 	//每个识别到的单词
-	String word=null;
+	String word;
 	//单词表，存放单词和词频
-	TreeMap<String, Integer> wordList=new TreeMap<String,Integer>();
+	final TreeMap<String, Integer> wordList=new TreeMap<String,Integer>();
 	char ch1;
 	char ch2;
 
@@ -18,42 +17,44 @@ public static TreeMap<String,Integer> parse(String filename) throws IOException{
 	try {
 		fileReader=new FileReader(new File(filename));
 		bufferedReader=new BufferedReader(fileReader);
-				
+		
 		while((str=bufferedReader.readLine())!=null){
 			startIndex=0;
 			endIndex=0;
-					
+			
 			for(int i=0;i<str.length();i++){
 				ch1=str.charAt(i);
-											
+									
 				//如果是第一个字母
-				if((startIndex==0)&&(ch1>=65&&ch1<=90||ch1>=97&&ch1<=122)){
+				if(startIndex==0&&(ch1>=65&&ch1<=90||ch1>=97&&ch1<=122)){
 					startIndex=i+1;
 				}
-						
+				
 				//第一个字母后面的，判断单词何处结束
 				if(startIndex!=0){
-							
+					
 					//字符是“-”
 					if(ch1==45){
 						if(i<str.length()-1){
 							ch2=str.charAt(i+1);
 							//“-”后面跟的不是字母
-							if(ch2<65||(ch2>90&&ch2<97)||ch2>122){
+							if(ch2<65||ch2>90&&ch2<97||ch2>122){
 								endIndex=i+1;
 							}
 						}
-						else endIndex=i+1;
+						else{
+							endIndex=i+1;
+						}
 					}
 					//此字符不是字母也不是“-”
-					else if(ch1<65||(ch1>90&&ch1<97)||ch1>122){
+					else if(ch1<65||ch1>90&&ch1<97||ch1>122){
 						endIndex=i+1;
 					}
 					//如果已到行末
 					else if(i==str.length()-1){
 						endIndex=i+2;
 					}
-							
+					
 					//如果单词结束
 					if(endIndex!=0){
 						//得到此单词（全部按小写）
@@ -66,21 +67,22 @@ public static TreeMap<String,Integer> parse(String filename) throws IOException{
 						else{
 							wordList.put(word, 1);
 						}
-									
+						
 						startIndex=0;
 						endIndex=0;
 					}
-				}
+				}	
 			}
 		}
-		return wordList;
-	} catch (Exception e) {
+		
+	} catch (FileNotFoundException e) {
 		JOptionPane.showMessageDialog(null, "文件不存在","  ", JOptionPane.INFORMATION_MESSAGE); 
 		System.exit(0);
+		
 	}finally{
 		//关闭文件流
 		bufferedReader.close();
 		fileReader.close();
 	}
-	return null;
+	return wordList;
 }
